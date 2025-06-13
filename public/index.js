8535abf2e94f805646c194e91ede21cd5b78af58
Junyuquick <junyuquick246@gmail.com>
2025-06-14 02:03:20 +0800

    ee

--- public/index.js
+++ public/index.js
@@ -3,4 +3,12 @@ const button = document.getElementById('startButton');
 
 button.addEventListener('click', function() {
     console.log("clicked once");
+    fetch('https://api.quickjunyu.com')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 });
